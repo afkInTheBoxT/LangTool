2d1996fdf6911d4f6eecc6cdcdabfaf25a174729
Yevhen_Sonin <ololosh40000@gmail.com>
2021-05-18 14:04:18 +0300

    Web folder added

---
+++ Web/script/script.js
@@ -0,0 +1,4 @@
+function startAuthorization(){
+    document.getElementsByClassName("sign-in-absolute-block_content")[0].style.display = "block";
+    document.getElementsByClassName("sign-in-absolute-block")[0].style.display = "block";
+}
