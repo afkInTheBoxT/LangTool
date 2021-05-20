75cd3b96c159aa799d0a8cc138fc10716ce7697f
Andrii Fedin <andrewfedin2@gmail.com>
2021-05-20 17:45:52 +0300

    Test connection

---
+++ ASP.NET/LangTool/LangTool/Models/LangToolContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LangTool.Models
+{
+    public class LangToolContext : DbContext
+    {        public DbSet<UserT> UsersT { get; set; }
+        public LangToolContext(DbContextOptions<LangToolContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
